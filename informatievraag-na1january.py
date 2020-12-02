3d78c7454ff681d618676f54852084ee39965357
Sam <sambrands2001@gmail.com>
2020-12-02 14:02:14 +0100

    Now only prints total permits after date

--- informatievraag-na1january.py
+++ informatievraag-na1january.py
@@ -7,9 +7,11 @@ data = list(reader)
 # hier onder staan de variable
 datetime_str = '1-1-2020'
 datetime_object = datetime.strptime(datetime_str, '%d-%m-%Y')
-afterFile = len(data)
+permitsAfterDate = 0
 
 # hier onder staan de statements
-for license in data:
-    if (datetime.strptime(license['Datum_aanvraag'], '%d-%m-%Y') >= datetime_object):
-        print(f"{license['Datum_aanvraag']}")
+for permit in data:
+   if (datetime.strptime(permit['Datum_aanvraag'], '%d-%m-%Y') >= datetime_object):
+      permitsAfterDate += 1
+   
+print(permitsAfterDate)
